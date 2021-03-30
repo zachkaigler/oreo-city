bf32f56781e2aa12e2a5163e3848b8be3504d2c1
William Zembricki <wzembricki@gmail.com>
2021-03-30 14:12:46 -0400

    frontend comment functional

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ let oreoBar = document.querySelector("div#oreo-header.header")
 let oreoMainImg = document.querySelector("img#placeholder-img")
 let commentForm = document.querySelector("form#comment-form")
 let expertRatingH2 = document.querySelector("h2#expert-rating")
-
-fetch("http://localhost:3000/oreos")
+let commentValue= document.querySelector("input#comment")
+let nameValue= document.querySelector("input#name")
+let commentList = document.querySelector("ul#comment-list")
+let currentOreo = {}
+fetch("http://localhost:3000/oreos?_embed=comments")
     .then(resp => resp.json())
     .then(function(oreoArray) {
         oreoArray.forEach(function(oreoObj) {
@@ -21,8 +24,56 @@ fetch("http://localhost:3000/oreos")
                 oreoMainImg.alt = oreoObj.flavor
                 commentForm.dataset.id = oreoObj.id
                 expertRatingH2.innerText = `Expert Rating: ${oreoObj.expertRating}`
+                currentOreo = oreoObj
+                commentList.innerHTML = ""
+                oreoObj.comments.forEach(function(commentObj){
+                    let blankLi = document.createElement("li")
+            
+                    let blankH = document.createElement("h3")
+                    blankH.innerText = commentObj.name
+                    let blankP = document.createElement("p")
+                    blankP.innerText = commentObj.comment
 
-                
+                    blankLi.append(blankH, blankP)
+                    commentList.append(blankLi)
+
+
+
+                })
+
+               
             })
         })
+    })
+
+    commentForm.addEventListener("submit", function(evt){
+        evt.preventDefault()
+        let ID = currentOreo.id
+        fetch(`http://localhost:3000/comments`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                oreoId: ID,
+                name: nameValue.value,
+                comment: commentValue.value
+            })
+        })
+        .then(res => res.json())
+        .then(function(updatedComment){
+            let blankLi = document.createElement("li")
+            
+            let blankH = document.createElement("h3")
+            blankH.innerText = updatedComment.name
+            let blankP = document.createElement("p")
+            blankP.innerText = updatedComment.comment
+
+            blankLi.append(blankH, blankP)
+            commentList.append(blankLi)
+
+        })
+
+
+
     })
